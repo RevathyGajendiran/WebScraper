f94d550b2f4ab368c0d71c72250448153eddc2d8
RevathyGajendiran <revathy9677@gmail.com>
2017-12-14 10:31:31 +0900

    Accessing the table
    Printing the table header in the first table

--- web2.py
+++ web2.py
@@ -1,4 +1,4 @@
-#printing the heading in the webpage
+#Accessing the table header of the table in the webpage.
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 my_url='https://en.wikipedia.org/wiki/List_of_districts_in_Tamil_Nadu'
@@ -6,4 +6,7 @@ uClient=uReq(my_url)
 page_html=uClient.read()
 uClient.close()
 page_soup=soup(page_html,"html.parser")
-print (page_soup.h1)
+name_box = page_soup.find('table', class_='wikitable sortable')
+city=name_box.tr
+city2=city.text
+print(city2)
