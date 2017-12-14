9bdb4de557668e781cdfba9c90d10b689b77e5de
RevathyGajendiran <revathy9677@gmail.com>
2017-12-14 10:35:20 +0900

    Extracting the district details table into an excel file
    Pandas lib is added knowing since can be easily stored as dataframes and can be easily exported to an excel file.

--- web2.py
+++ web2.py
@@ -1,12 +1,16 @@
-#Accessing the table header of the table in the webpage.
+#extracting the district table into excel
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
+import pandas as pd 
+import xlrd
 my_url='https://en.wikipedia.org/wiki/List_of_districts_in_Tamil_Nadu'
 uClient=uReq(my_url)
 page_html=uClient.read()
 uClient.close()
 page_soup=soup(page_html,"html.parser")
-name_box = page_soup.find('table', class_='wikitable sortable')
-city=name_box.tr
-city2=city.text
-print(city2)
+#city_tables = page_soup.find_all('table', class_='wikitable sortable')
+#name = name_box.text.strip() # strip() is used to remove starting and trailing
+#for i in range(1,10):
+pd.set_option('max_columns', 9)
+citydetails = pd.read_html(my_url)[2]
+citydetails.to_excel('citydetails.xlsx', index=False)
